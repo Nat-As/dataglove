ed496083def133af140cf09b31914ca22cb7a820
James Andrews <42288915+Nat-As@users.noreply.github.com>
2019-08-06 09:53:57 -0400

    2iC adaptor

---
+++ gloveadaptor.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/*This code serves as a dummy function for testing
+ * The real adaptor will not be published untill it is finished and permission is given by 2iC
+ * James Andrews <jandrews7348@floridapoly.edu>
+ */
+ 
+ int main(int argc, char *argv[]){
+	 puts(argv[1]);//the data that will be sent by the adaptor
+ } 
